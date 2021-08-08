ddd9b3dbdc98602c1710df85c69a58f119cf5340
JP109 <jaipawar113@gmail.com>
2021-08-08 12:11:24 +0530

    Base object detection implementation with toast for detected phones

--- src/App.js
+++ src/App.js
@@ -1,22 +1,138 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import Webcam from 'react-webcam';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd"
+import { drawRect } from "./utilities";
+
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const notify = () => toast.dark('Phone detected!');
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network 
+    const net = await cocossd.load();
+    
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  let obj, phone
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      obj = await net.detect(video);
+      if(obj && obj.length>0){
+        // phone = obj.filter(o=>{o.class==='cell phone' return );
+        // if(phones>0){
+          // notify();
+        //   console.log(phones)
+        // }
+        phone = obj.find((object)=>{
+          if(object.class==='cell phone')
+          return true;
+        });
+      }
+
+      if(phone){
+        notify();
+      }
+      
+      // console.log("Model output:",obj);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+
+      // 5. TODO - Update drawing utility
+      drawRect(obj, ctx)  
+    }
+  };
+
+  useEffect(()=>{runCoco()},[]);
+
+  // const notify = () => toast.dark('Court Added!');
+  // let phones = 0;
+  // useEffect(() => {
+  //   if(obj && obj.length>0){
+  //     phones = obj.filter(o=>o.class==='cell phone');
+  //     if(phones>0){
+  //       notify();
+  //       console.log(phones)
+  //     }
+  //   }
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
